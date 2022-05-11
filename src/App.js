60d29e6687fee9805317fdcf4463999b739502ab
Salah Zanabili <Zanabilis@gmail.com>
2022-05-11 04:57:15 -0500

    hacky drummachine

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React,{useState} from "react";
+import React from "react";
 import './App.css';
-import ButtonDrum from "./Components/ButtonDrum";
 import MyErrorBoundary from "./MyErrorBoundary";
 
 const App=()=>{
     //importing audio clips, label is the name for the key, displaydescription is for the name of the clip, and file link is the link
-    const audioClips=[
+    /*const audioClips=[
         {label: "Q", displayDescription:"amongus", fileLink:"https://drive.google.com/uc?export=download&id=1cQybTYGAU7h2cvSCuIuDR0jU51FXmGfe"},
         {label:"W", displayDescription:"bingchilling", fileLink:"https://drive.google.com/uc?export=download&id=19mxQPg9-7sbNDv25QElzM9NoZN5dCfY9"},
         {label:"E", displayDescription:"carrot", fileLink:"https://drive.google.com/uc?export=download&id=1YlMKfsjAAH-nLWRXLfguZjAzQfLuojgZ"},
@@ -16,18 +15,108 @@ const App=()=>{
         {label:"X", displayDescription:"wow", fileLink:"https://drive.google.com/uc?export=download&id=1IG082FEfx73n5PEUUFihdjlLK5JqywyR"},
         {label:"C", displayDescription:"yo", fileLink:"https://drive.google.com/uc?export=download&id=1JIqPwLjwF2Gx7pXKAf2Iw0y12F0EnmTb"},
     ];
+    */
+    const labels=["Q","W","E","A","S","D","Z","X","C"];
+    const playAudioClips=(displayDescription,label)=>{
+        document.getElementById("display").innerText=displayDescription;
+        let newClip=document.getElementById(label);
+        newClip.play();
+    }
+    const handleKeyPress=(event)=>{
+        for(let i=0;i<labels.length;i++){
+            if(event.keyCode===labels[i].charCodeAt(0)){
+                playAudioClips(Math.random().toString(),labels[i]);
+            }
+        }
+    }
+    React.useEffect(()=>{
+        document.addEventListener("keydown", handleKeyPress);
+        return()=>{
+            document.removeEventListener("keydown",handleKeyPress);
+        }
+    });
     //used for updating the div with the display description as text
-    const [displayDescription, setDisplayDescription]=useState("Click a button!");
+    
+    
     return(
         //maps each audio clip to a button, unique key is the label, each audio clip object is passed into the button, and the setDisplayDescription function is sent in so that when a button is clicked, it updates on screen
     <div id= "drum-machine">
         <MyErrorBoundary>
         <h1>meme soundboard</h1>
-        <p id="display">{displayDescription.toString()}</p>
+        <div id="display"></div>
         <div id="buttonContainer">
-            {audioClips.map((clip)=>(
-                <ButtonDrum setDisplayDescription={setDisplayDescription} key= {clip.label} clip={clip} />
-            ))}
+            <button 
+                className="drum-pad btn btn-primary p-4 m-3"
+                id="amongus"
+                onClick= {()=>playAudioClips("amongus","Q")}
+            >
+                <audio className="clip" id="Q" src="https://drive.google.com/uc?export=download&id=1cQybTYGAU7h2cvSCuIuDR0jU51FXmGfe"></audio>
+                Q
+            </button>
+            <button 
+                className="drum-pad btn btn-primary p-4 m-3"
+                id="bingchilling"
+                onClick= {()=>playAudioClips("bingchilling","W")}
+            >
+                <audio className="clip" id="W" src="https://drive.google.com/uc?export=download&id=19mxQPg9-7sbNDv25QElzM9NoZN5dCfY9"></audio>
+                W
+            </button>
+            <button 
+                className="drum-pad btn btn-primary p-4 m-3"
+                id="carrot"
+                onClick= {()=>playAudioClips("carrot","E")}
+            >
+                <audio className="clip" id="E" src="https://drive.google.com/uc?export=download&id=1YlMKfsjAAH-nLWRXLfguZjAzQfLuojgZ"></audio>
+                E
+            </button>
+            <button 
+                className="drum-pad btn btn-primary p-4 m-3"
+                id="china"
+                onClick= {()=>playAudioClips("china","A")}
+            >
+                <audio className="clip" id="A" src="https://drive.google.com/uc?export=download&id=1Fpou-9zit_5uppV6aEzVehPqTW9_o2wb"></audio>
+                A
+            </button>
+            <button 
+                className="drum-pad btn btn-primary p-4 m-3"
+                id="nani"
+                onClick= {()=>playAudioClips("nani","S")}
+            >
+                <audio className="clip" id="S" src="https://drive.google.com/uc?export=download&id=1Jlt0LNiwCDpov-Z6DTF48OBuoowU8Jbx"></audio>
+                S
+            </button>
+            <button 
+                className="drum-pad btn btn-primary p-4 m-3"
+                id="sharingan"
+                onClick= {()=>playAudioClips("sharingan","D")}
+            >
+                <audio className="clip" id="D" src="https://drive.google.com/uc?export=download&id=1f908gDa2ekFzbGHUNhy00CB12oUIwFfI"></audio>
+                D
+            </button>
+            <button 
+                className="drum-pad btn btn-primary p-4 m-3"
+                id="uwu"
+                onClick= {()=>playAudioClips("uwu","Z")}
+            >
+                <audio className="clip" id="Z" src="https://drive.google.com/uc?export=download&id=1DUCmYRFzSYfZeaf8mSVmxOcDWwHVBWCE"></audio>
+                Z
+            </button>
+            <button 
+                className="drum-pad btn btn-primary p-4 m-3"
+                id="wow"
+                onClick= {()=>playAudioClips("wow","X")}
+            >
+                <audio className="clip" id="X" src="https://drive.google.com/uc?export=download&id=1IG082FEfx73n5PEUUFihdjlLK5JqywyR"></audio>
+                X
+            </button>
+            <button 
+                className="drum-pad btn btn-primary p-4 m-3"
+                id="yo"
+                onClick= {()=>playAudioClips("yo","C")}
+            >
+                <audio className="clip" id="C" src="https://drive.google.com/uc?export=download&id=1JIqPwLjwF2Gx7pXKAf2Iw0y12F0EnmTb"></audio>
+                C
+            </button>
         </div>
         </MyErrorBoundary>
     </div>);
